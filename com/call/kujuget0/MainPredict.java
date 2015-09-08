babfc2801414a5cffd9968cbaf36bd53957d748c
akuzhuget <akuzhuget@outlook.com>
2015-09-07 23:29:55 -0400

    Update MainPredict.java

--- com/call/kujuget0/MainPredict.java
+++ com/call/kujuget0/MainPredict.java
@@ -5,29 +5,54 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainPredict {	
-	
+	class MyTask implements Runnable {
+		String name;
+		public MyTask(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public void run() {
+			BSPredictor bs = new BSPredictor();
+			bs.SetFilePath(name);
+			bs.SetName(name);
+			//System.out.println(bs.linearStrategy());
+			//System.out.println(bs.lastStrategy());
+			bs.predict();
+			System.out.println(bs.getRealMoney());
+		}
+	}
+
 	static FileFilter filter = new FileFilter() {
 		@Override
 		public boolean accept(File pathname) {
 			return pathname.getName().toLowerCase().endsWith("csv");
 		}
 	};
-     
-    public static void main(String[] args) {
-    	String fileName="OptionData";
-    	String fileExtension=".csv";    	
-    	int startIndex=6, endIndex=6;
-    	BSPredictor bs = new BSPredictor();
-    	File dir = new File(Util.Path);
-    	File[] files = dir.listFiles(filter);
-    	for (File file:files) {
-    		//String file = Util.Path + fileName + String.valueOf(i) + fileExtension;
-    		bs.SetFilePath(file.toString());
-    		bs.SetName(file.toString());    		
-			System.out.println(bs.predict().toStringBuffer());
+
+	public void doMain() throws InterruptedException {
+		String fileName="OptionData";
+		String fileExtension=".csv";
+		int startIndex=6, endIndex=6;
+		BSPredictor bs = new BSPredictor();
+		File dir = new File(Util.Path);
+		File[] files = dir.listFiles(filter);
+		ExecutorService es = Executors.newFixedThreadPool(1);
+		for (int i=0; i<files.length; i++) {
+			File file = files[i];
+			//String file = Util.Path + fileName + String.valueOf(i) + fileExtension;
+			es.submit(new MyTask(file.toString()));
 		}
-    	System.out.println("Done");
-    }    
+		es.shutdown();
+		es.awaitTermination(1, TimeUnit.DAYS);
+		System.out.println("Done with upward slopped " + BSPredictor.upward);
+	}
+    public static void main(String[] args) throws InterruptedException {
+		new MainPredict().doMain();
+   }
 }
